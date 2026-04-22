6f9cb3aef0e1ae47d03dff5a98bc334155528fae
altadmin01-collab <altadmin01@gmail.com>
2026-04-22 22:02:34 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,7 @@ export default function Page() {
   return (
     <div style={{ padding: 40 }}>
       <h1>🎉 成功了！你的产品上线了</h1>
-
-      <p>这是你第一个真正部署成功的页面</p>
+      <p>这说明部署已经完全成功</p>
 
       <button
         onClick={() => setCount(count + 1)}
